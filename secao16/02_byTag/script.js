a6771f37de1f129fff55afa43ca80d9421456ed9
sheltonFarias <sheltonm10@outlook.com>
2024-06-04 15:33:51 -0300

    add secao 16

---
+++ secao16/02_byTag/script.js
@@ -0,0 +1,5 @@
+console.log(document.getElementsByTagName('h1')) // Retorna todos os h1 presentes
+
+console.log(document.getElementsByTagName('p')) // Retorna todos os p presentes
+
+console.log(document.getElementsByTagName('li')) // Retorna todos os li presentes
---
+++ secao16/05_query/script.js
@@ -0,0 +1,1 @@
+console.log(document.querySelector('#container-principal h1')) // Acessando o h1 que esta dentro da div com o id container
